4c27eb62aaa6fd478872b81bf0d9db87f29d3f26
Imran Khan <imrankhan772244@gmail.com>
2024-03-08 22:19:45 +0530

    mongoo update

--- Contact_list/index.js
+++ Contact_list/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // getting mongo db in our project
 const db=require('./config/mongoose')
-
+const Contact = require('./models/contact')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
@@ -37,7 +37,16 @@ var contactList = [
 ];
 
 app.get('/', function (req, res) {
-    return res.render('home', { title: 'I am Iron!', Contact_List: contactList });
+
+    Contact.find({})
+    .then(contacts => {
+        console.log(contacts);
+        return res.render('home', { title: 'Contact List', Contact_List: contacts });
+    })
+    .catch(err => {
+        console.log("error in fetching contacts", err);
+        return res.status(500).send("Error in fetching contacts");
+    });
 });
 
 app.get('/play', function (req, res) {
@@ -45,21 +54,42 @@ app.get('/play', function (req, res) {
 });
 
 app.post('/create-contact',function(req,res){
-    console.log(req.body);
-    contactList.push(req.body);
-    return res.redirect('back');
+    // console.log(req.body);
+    // contactList.push(req.body);
+    // return res.redirect('back');
+
+    Contact.create({
+        name: req.body.name,
+        phone: req.body.phone
+    })
+    .then(newContact => {
+        console.log('********', newContact);
+        return res.redirect('/');
+    })
+    .catch(err => {
+        console.log("error in creating new contact", err);
+        return res.status(500).send("Error in creating new contact");
+    });
 });
 
 
 app.get('/delete-contact/',function(req,res){
-    console.log(req.query)
-    console.log(req.query.phone)
-   const index= contactList.findIndex((item)=> item.phone==req.query.phone)
-   if(index != -1){
-    contactList.splice(index,1);
-    return res.redirect('back');
-   }
-   return;
+//     console.log(req.query)
+//     console.log(req.query.phone)
+//    const index= contactList.findIndex((item)=> item.phone==req.query.phone)
+//    if(index != -1){
+//     contactList.splice(index,1);
+//     return res.redirect('back');
+//    }
+//    return;
+    let id=req.query.id;
+    Contact.findByIdAndDelete(id)
+    .then(()=>{
+        return res.redirect('back');
+    }).catch((err)=>{
+        console.log('Error While deleting this contact!!!')
+        return;
+    })
 })
 
 
