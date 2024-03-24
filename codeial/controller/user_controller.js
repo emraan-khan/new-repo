b5b6e094cd1a8274b3054d9a86a58fae0e6233c0
Imran Khan <imrankhan772244@gmail.com>
2024-03-24 22:08:58 +0530

    signIn,Up commit

--- codeial/controller/user_controller.js
+++ codeial/controller/user_controller.js
@@ -6,3 +6,21 @@ module.exports.user = function(req, res){
 module.exports.orderRes = function(req, res){
     return res.end('<h1>Muh mai lele!!!</h1>')
 }
+
+module.exports.signIn = function(req,res){
+    return res.render('user_signIn');
+}
+
+module.exports.signUp = function(req,res){
+    return res.render('user_signUp');
+}
+
+// create action for user controller
+module.exports.create = function(req,res){
+    //TODO
+}
+
+//sign in and create session for user
+module.exports.createSession = function(req, res){
+
+}
--- codeial/routes/user.js
+++ codeial/routes/user.js
@@ -6,6 +6,10 @@ const userController = require('../controller/user_controller');
 
 router.get('/profile',userController.user);
 
-router.get('/order',userController.orderRes)
+router.get('/order',userController.orderRes);
+
+router.get('/signIn',userController.signIn);
+
+router.get('/signUp',userController.signUp);
 
 module.exports= router;
